edf221a07c9b971e6ebbf724c8256e15ece3c64a
yusfia <yusfia.hafidz@gmail.com>
2018-03-17 13:09:08 +0700

    firebase preparation

---
+++ app/src/main/java/id/smartin/org/homecaretimedic/service/FireBaseInstanceIDService.java
@@ -0,0 +1,37 @@
+package id.smartin.org.homecaretimedic.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class FireBaseInstanceIDService extends FirebaseInstanceIdService {
+    public static final String TAG = "[FireBaseInstanceID]";
+    public FireBaseInstanceIDService() {
+    }
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // TODO: Implement this method to send any registration to your app's servers.
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    /**
+     * Persist token to third-party servers.
+     *
+     * Modify this method to associate the user's FCM InstanceID token with any server-side account
+     * maintained by your application.
+     *
+     * @param token The new token.
+     */
+    private void sendRegistrationToServer(String token) {
+        // Add custom implementation, as needed.
+    }
+}
