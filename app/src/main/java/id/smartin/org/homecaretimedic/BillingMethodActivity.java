95f3c16ca303389d5291e549081de32ec3c39eac
yusfia <yusfia.hafidz@gmail.com>
2018-04-12 16:57:52 +0700

    RecyclerView on billing

--- app/src/main/java/id/smartin/org/homecaretimedic/BillingMethodActivity.java
+++ app/src/main/java/id/smartin/org/homecaretimedic/BillingMethodActivity.java
@@ -4,13 +4,24 @@ import android.annotation.SuppressLint;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.widget.RelativeLayout;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import id.smartin.org.homecaretimedic.adapter.BillingItemAdapter;
+import id.smartin.org.homecaretimedic.adapter.CaregiverHistoryAdapter;
+import id.smartin.org.homecaretimedic.adapter.PaymentMethodAdapter;
 import id.smartin.org.homecaretimedic.model.BillingItem;
+import id.smartin.org.homecaretimedic.model.PaymentMethod;
+import id.smartin.org.homecaretimedic.model.submitmodel.SubmitInfo;
 import id.smartin.org.homecaretimedic.tools.ViewFaceUtility;
 
 public class BillingMethodActivity extends AppCompatActivity {
@@ -21,9 +32,15 @@ public class BillingMethodActivity extends AppCompatActivity {
     @BindView(R.id.toolbar)
     Toolbar toolbar;
     @BindView(R.id.billingItems)
-    RecyclerView billingItems;
+    RecyclerView billingItemsRV;
     @BindView(R.id.billingMethods)
-    RecyclerView billingMethods;
+    RecyclerView billingMethodsRV;
+
+    private PaymentMethodAdapter paymentMethodAdapter;
+    private List<PaymentMethod> paymentMethods = new ArrayList<>();
+
+    private BillingItemAdapter billingItemAdapter;
+    private List<BillingItem> billingItems = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,10 +48,44 @@ public class BillingMethodActivity extends AppCompatActivity {
         setContentView(R.layout.activity_billing_method);
         ButterKnife.bind(this);
         createTitleBar();
+
+        DividerItemDecoration dividerItemDecorationBI = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+        final RecyclerView.LayoutManager mLayoutManagerBI = new LinearLayoutManager(getApplicationContext());
+        DividerItemDecoration dividerItemDecorationBM = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+        final RecyclerView.LayoutManager mLayoutManagerBM = new LinearLayoutManager(getApplicationContext());
+
+        billingItemAdapter = new BillingItemAdapter(this, this, billingItems);
+        billingItemsRV.addItemDecoration(dividerItemDecorationBI);
+        billingItemsRV.setLayoutManager(mLayoutManagerBI);
+        billingItemsRV.setItemAnimator(new DefaultItemAnimator());
+        billingItemsRV.setAdapter(billingItemAdapter);
+
+        paymentMethodAdapter = new PaymentMethodAdapter(this, this, paymentMethods);
+        billingMethodsRV.addItemDecoration(dividerItemDecorationBM);
+        billingMethodsRV.setLayoutManager(mLayoutManagerBM);
+        billingMethodsRV.setItemAnimator(new DefaultItemAnimator());
+        billingMethodsRV.setAdapter(paymentMethodAdapter);
+        getPaymentMethod();
+        getBillingItems();
     }
 
-    private void dataFilling(){
+    private void getBillingItems(){
+        BillingItem a = new BillingItem();
+        a.setItem(SubmitInfo.selectedHomecareService.getServiceName());
+        a.setPrice(SubmitInfo.selectedHomecareService.getVisitCost());
+        BillingItem b = new BillingItem();
+        b.setItem("Perlengkapan");
+        b.setPrice(SubmitInfo.getAssessmentPrice());
+        billingItems.add(a);
+        billingItems.add(b);
+    }
 
+    private void getPaymentMethod(){
+        PaymentMethod a = new PaymentMethod();
+        a.setDescription("Transfer Bank");
+        a.setPaymentMethodName("TransferBank");
+        paymentMethods.add(a);
+        paymentMethodAdapter.notifyDataSetChanged();
     }
 
     @SuppressLint("RestrictedApi")
